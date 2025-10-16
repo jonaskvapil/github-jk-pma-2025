e2336937f797b2bb998a9121007c2a9529c845ac
jonda <jonas.kvapi@tul.cz>
2025-10-16 11:26:10 +0200

    #07

---
+++ 07/app/src/main/java/com/example/a07/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.a07
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.a07.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+        fragmentTransaction.commit()
+    }
+}
